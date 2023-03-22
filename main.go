619ba0ff61923e29fab5d624395a9516617db01d
amir.ch.charehei1382 <amir.ch.charehei1382@gmail.com>
2023-03-22 21:15:21 +0430

    add server.go to serving on http protocol

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
-import (
-	"localPackage/lib"
-)
-
 func main() {
-	product := lib.Crawl("https://www.nike.com/t/air-max-plus-mens-shoes-x9G2xF/604133-139")
-	lib.ToCSV(product, "./nike_product.json")
+
+	// running server
+	Server()
 }
---
+++ server.go
@@ -0,0 +1,43 @@
+package main 
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"io"
+	"encoding/json"
+	"log"
+
+	"localPackage/lib"
+	
+)
+
+func returnData(dataBytes io.Reader) map[string]interface{} {
+	data := make(map[string]interface{})
+	posted_data, err := ioutil.ReadAll(dataBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(posted_data, &data)
+	return data
+}
+
+func Server() {
+	fmt.Println("server is running")
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	router.POST("/nike", NikeProductHandler)
+
+	router.Run("0.0.0.0:8080")
+
+}
+
+func NikeProductHandler(context *gin.Context) {
+	data := returnData(context.Request.Body)
+
+	nikeURL := data["url"].(string)
+	product := lib.Crawl(nikeURL)
+
+	context.JSON(200, product)
+}
